70786f7e9102c6040bc5c5368d7dbadf4453e2bb
jms <jm@audio.d3ck.net>
2017-11-20 17:13:26 +0100

    fixed minor lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
     "parserOptions": {
         "sourceType": "module"
-    }
+    },
     "rules": {
         "no-cond-assign": 0,
         "indent-legacy": 0,
--- build/update.js
+++ build/update.js
@@ -1,16 +1,11 @@
+/* eslint no-console: 0 */
 var fs = require('fs');
 var current = require('../completions.json');
 // file paths
-var fname_css = "uikit/dist/css/uikit.css";
-var fname_js = "uikit/dist/js/uikit.js";
-var outfile = "completions.json";
+var fname_css = 'uikit/dist/css/uikit.css';
+var fname_js = 'uikit/dist/js/uikit.js';
+var outfile = 'completions.json';
 
-if (!fs.existsSync(fname_css) || !fs.existsSync(fname_js)) {
-    console.log("Can't locate UIkit files");
-    return;
-}
-
-// returns the array with all duplicate elements removed
 function remove_duplicates(arr) {
     var _set = {};
     arr.forEach(function(el) { _set[el] = true; });
@@ -21,42 +16,44 @@ function remove_duplicates(arr) {
 function css() {
     var src = fs.readFileSync(fname_css).toString();
     var classes = src.match(/\.(uk-[a-z\d\-]+)/g);
-        classes = remove_duplicates(classes).map(function(cls) { return cls.substr(1); }); // remove leading dot
+    classes = remove_duplicates(classes).map(function(cls) { return cls.substr(1); }); // remove leading dot
 
     return classes;
 }
 
 // attributes: uk-* from JS
 function js() {
-    var src   = fs.readFileSync(fname_js).toString(),
-        attrs = src.match(/uk-[a-z\d\-]+/g),
-        attrs = remove_duplicates(attrs);
-    return attrs;
+    var src = fs.readFileSync(fname_js).toString();
+    var attrs = src.match(/uk-[a-z\d\-]+/g);
+    return remove_duplicates(attrs);
 }
 
 function atom() {
     var _css = css(),
-        _js  = js(),
+        _js = js(),
         lst = _css.concat(_js);
 
-
     lst = remove_duplicates(lst);
- 
-    lst = lst.sort((a, b) => a < b ? -1 : 1 );
+
+    lst = lst.sort((a, b) => a < b ? -1 : 1);
 
     lst.forEach(cls => {
-        if(current.indexOf(cls) < 0) {
+        if (current.indexOf(cls) < 0) {
             console.log('added:', cls);
         }
     });
     current.forEach(cls => {
-        if(lst.indexOf(cls) < 0) {
+        if (lst.indexOf(cls) < 0) {
             console.log('removed:', cls);
         }
     });
-        
+
     fs.writeFileSync(outfile, JSON.stringify(lst, null, 4));
-    console.log("Done.");
+    console.log('Done.');
 }
 
-atom();
+if (fs.existsSync(fname_css) && fs.existsSync(fname_js)) {
+    atom();
+} else {
+    console.log("Can't locate UIkit files");
+}
