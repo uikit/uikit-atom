9f1a90d07d2a3717d0aba4388d8727e22d7fe0ac
janschoenherr <jan@yootheme.com>
2017-11-22 10:30:02 +0100

    cleanup

--- build/update.js
+++ build/update.js
@@ -1,59 +1,34 @@
 /* eslint no-console: 0 */
 var fs = require('fs');
 var current = require('../completions.json');
+
 // file paths
-var fname_css = 'uikit/dist/css/uikit.css';
-var fname_js = 'uikit/dist/js/uikit.js';
-var outfile = 'completions.json';
+var css = 'uikit/dist/css/uikit.css';
+var js = 'uikit/dist/js/uikit.js';
 
-function remove_duplicates(arr) {
-    var _set = {};
-    arr.forEach(function(el) { _set[el] = true; });
-    return Object.keys(_set);
+if (!fs.existsSync(css) || !fs.existsSync(js)) {
+    console.log('Can\'t locate UIkit files.');
 }
 
-// classes: uk-* from CSS
-function css() {
-    var src = fs.readFileSync(fname_css).toString();
-    var classes = src.match(/\.(uk-[a-z\d\-]+)/g);
-    classes = remove_duplicates(classes).map(function(cls) { return cls.substr(1); }); // remove leading dot
+var list = merge(
+        read(css).match(/\.uk-[a-z\d-]+/g).map(cls => cls.substr(1)),
+        read(js).match(/uk-[a-z\d-]+/g)
+    );
 
-    return classes;
+list.forEach(item => !~current.indexOf(item) && console.log('added:', item));
+current.forEach(item => !~list.indexOf(item) && console.log('removed:', item));
+
+fs.writeFileSync('completions.json', JSON.stringify(list, null, 4));
+console.log('Done.');
+
+function merge(...arr) {
+    return Array.from(
+        new Set(
+            Array.prototype.concat.apply(arr[0], arr.slice(1))
+        )
+    ).sort();
 }
 
-// attributes: uk-* from JS
-function js() {
-    var src = fs.readFileSync(fname_js).toString();
-    var attrs = src.match(/uk-[a-z\d\-]+/g);
-    return remove_duplicates(attrs);
-}
-
-function atom() {
-    var _css = css(),
-        _js = js(),
-        lst = _css.concat(_js);
-
-    lst = remove_duplicates(lst);
-
-    lst = lst.sort((a, b) => a < b ? -1 : 1);
-
-    lst.forEach(cls => {
-        if (current.indexOf(cls) < 0) {
-            console.log('added:', cls);
-        }
-    });
-    current.forEach(cls => {
-        if (lst.indexOf(cls) < 0) {
-            console.log('removed:', cls);
-        }
-    });
-
-    fs.writeFileSync(outfile, JSON.stringify(lst, null, 4));
-    console.log('Done.');
-}
-
-if (fs.existsSync(fname_css) && fs.existsSync(fname_js)) {
-    atom();
-} else {
-    console.log("Can't locate UIkit files");
+function read(file) {
+    return fs.readFileSync(file).toString();
 }
